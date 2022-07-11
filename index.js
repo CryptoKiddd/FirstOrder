7b7383aec23e30d252ecfdf96b8c19be9719ffaa
armaz avaliani <avaliani1232@gmail.com>
2022-07-11 20:29:59 +0400

    animations on section-info-products

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ list.forEach((item)=>item.addEventListener('mouseover',addClass));
 
 
 // //////////////////animations/////////////
+// gsap.registerPlugin(ScrollTrigger)
 
 
 
 //navigation and header animation
-gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:2})
+gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:3,ease:'bounce'})
 .from('.header-content',{opacity:0, scale:0, duration:1, ease:"circ.out"})
 .from('.list',{y:160,stagger:0.1,duration:0.8, ease:'back',opacity:0});
 
@@ -31,17 +32,33 @@ gsap.from('.pih-h2',
 scrollTrigger:
 {
     trigger:'.pih-h2',
-    toggleActions:"restart pause none none"
+    toggleActions:"restart none none none"
 
 },
 opacity:0,scale:0,duration:2,x:-400});
-gsap.from('.curtains-right-p',{
+
+ gsap.from('.crp-p-animation',{
     scrollTrigger:
-        {trigger:'.curtains-right-p',
-        toggleActions:"restart pause none none"
+        {trigger:'.products-info-header',
+        toggleActions:"restart none none none"
     
     },
-    y:400,opacity:0,duration:1});
+    y:400,opacity:0,duration:3});
+
+ gsap.from('.prodcuts-info-curtain-img',{
+    scrollTrigger:
+        {trigger:'.products-info-header',
+        toggleActions:"restart none none none"
+    
+    },
+    opacity:0,x:200, duration:2,ease:'none',});
+ gsap.from('.crp-header',{
+    scrollTrigger:
+        {trigger:'.products-info-header',
+        toggleActions:"restart none none none"
+    
+    },
+    opacity:0,y:-250, duration:3,});
 
 
 
