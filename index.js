87925d4b69c559eecada5bc88cef57fee03954aa
armaz avaliani <avaliani1232@gmail.com>
2022-07-11 19:32:52 +0400

    animations-started

--- index.js
+++ index.js
@@ -17,7 +17,40 @@ list.forEach((item)=>item.addEventListener('mouseover',addClass));
 
 // //////////////////animations/////////////
 
+
+
+//navigation and header animation
 gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:2})
 .from('.header-content',{opacity:0, scale:0, duration:1, ease:"circ.out"})
 .from('.list',{y:160,stagger:0.1,duration:0.8, ease:'back',opacity:0});
-gsap.timeline.from('.pih-h2',{opacity:0,scale:0})
+
+
+
+gsap.from('.pih-h2',
+{
+scrollTrigger:
+{
+    trigger:'.pih-h2',
+    toggleActions:"restart pause none none"
+
+},
+opacity:0,scale:0,duration:2,x:-400});
+gsap.from('.curtains-right-p',{
+    scrollTrigger:
+        {trigger:'.curtains-right-p',
+        toggleActions:"restart pause none none"
+    
+    },
+    y:400,opacity:0,duration:1});
+
+
+
+//scroll animation
+const pdfButton = document.getElementById('#scroll-pdf')
+
+
+
+
+
+
+
