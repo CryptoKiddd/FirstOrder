1fc0ddc30e7570e5605d609ba3ca8e2a3c6fd160
armaz avaliani <avaliani1232@gmail.com>
2022-07-11 09:27:31 +0400

    gsap-intro

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 const list = document.querySelectorAll('.list');
+
 console.log(list)
 function addClass(){
     list.forEach((item)=>{
@@ -11,10 +13,11 @@ function addClass(){
 
 list.forEach((item)=>item.addEventListener('mouseover',addClass));
 
-const config ={
-  type:'carousel',
-  perView:5,
- 
-}
-new Glide('.glide',config).mount();
-gsap.to()
+
+
+// //////////////////animations/////////////
+
+gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:2})
+.from('.header-content',{opacity:0, scale:0, duration:1, ease:"circ.out"})
+.from('.list',{y:160,stagger:0.1,duration:0.8, ease:'back',opacity:0});
+gsap.timeline.from('.pih-h2',{opacity:0,scale:0})
