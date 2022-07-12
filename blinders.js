e7d7027b61647f0e10424ecc0e968ee491747323
armaz avaliani <avaliani1232@gmail.com>
2022-07-12 07:32:03 +0400

    animations

--- blinders.js
+++ blinders.js
@@ -4,9 +4,9 @@ var swiper = new Swiper(".mySwiper", {
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-      rotate: 180,
-      stretch: 0,
-      depth: 400,
+      rotate: 100,
+      stretch: 1,
+      depth: 800,
       modifier: 1,
       slideShadows: true,
     },
--- index.js
+++ index.js
@@ -21,44 +21,74 @@ list.forEach((item)=>item.addEventListener('mouseover',addClass));
 
 
 //navigation and header animation
-gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:3,ease:'bounce'})
-.from('.header-content',{opacity:0, scale:0, duration:1, ease:"circ.out"})
+gsap.timeline().from('.navigation',{xPercent:100,opacity:0,duration:3,ease:'back'})
+.from('.header-content',{y:200,opacity:0, scale:0, duration:1.5, ease:"circ.out"})
 .from('.list',{y:160,stagger:0.1,duration:0.8, ease:'back',opacity:0});
 
 
 
+   
 gsap.from('.pih-h2',
 {
 scrollTrigger:
 {
     trigger:'.pih-h2',
-    toggleActions:"restart none none none"
+    start:'top bottom',
+    toggleActions:"restart none none none",
+    scrub:true,
 
 },
-opacity:0,scale:0,duration:2,x:-400});
+opacity:0,scale:0,duration:2,x:-400 });
 
  gsap.from('.crp-p-animation',{
     scrollTrigger:
-        {trigger:'.products-info-header',
-        toggleActions:"restart none none none"
+        {trigger:'.crp-p-animation',
+        start:'top bottom',
+        pin:true,
+        toggleActions:"restart none none none",
+       
     
     },
-    y:400,opacity:0,duration:3});
+    y:200,opacity:0,duration:3.5});
 
  gsap.from('.prodcuts-info-curtain-img',{
     scrollTrigger:
-        {trigger:'.products-info-header',
-        toggleActions:"restart none none none"
+        {trigger:'.prodcuts-info-curtain-img',
+        start:'top bottom',
+        toggleActions:"restart none none none",
+  
     
     },
-    opacity:0,x:200, duration:2,ease:'none',});
+    opacity:0,scale:0.01, duration:1.5,});
+
+
  gsap.from('.crp-header',{
     scrollTrigger:
-        {trigger:'.products-info-header',
-        toggleActions:"restart none none none"
+        {trigger:'.crp-header',
+        start:'top bottom',
+        toggleActions:"restart none none none",
+       
+        
     
     },
-    opacity:0,y:-250, duration:3,});
+    opacity:1,y:-250, duration:2,});
+
+
+ gsap.from('.curtains',{
+    scrollTrigger:
+        {trigger:'.catalog-h3',
+      
+        toggleActions:"restart none none none"},
+    opacity:0,x:350, duration:0.5,});
+
+ gsap.from('.blinders',{
+    scrollTrigger:
+        {trigger:'.catalog-h3',
+   
+      
+       
+        toggleActions:"restart none none none"},
+    opacity:1,x:-350,scale:0.1, duration:0.5, });
 
 
 
